38e2da0f62181aea467abe3b8879cf14ea05d275
tferd1 <78816019+tferd1@users.noreply.github.com>
2021-02-16 15:41:22 -0700

    Added Comments about Saga I dont know if the first PR worked so here's a second.

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -3,9 +3,37 @@ import { handleGetUser } from "./handlers/user";
 import { GET_USER } from "../ducks/user";
 
 //will look out for specific actions and map them to their handler functions
+//maybe change this to root saga and make it default
 export function* watcherSaga() {
-  yield takeLatest(GET_USER, handleGetUser);
-}
+  yield takeLatest(GET_USER, handleGetUser); //maybe change takeLatest here to yield all() and
+  //put the takeEvery in the individual functions
+  //MAYBE PASS IN AN ARRAY OF OBJECTS
+} //INSTEAD OF MAKING A BUNCH OF WATCHERS //BAHAHAHAH I WAS RIGHT
+
+//*****I THINK THIS ANSWERS EVERYTHING!!!******** */
+// function* helloSaga() {
+//   console.log('Hello Sagas!')
+// }
+
+// function* incrementAsync() {
+//   yield delay(1000)
+//   yield put({ type: 'INCREMENT' })
+// }
+
+// SO IM GONNA WANNA MAKE A BUNCH OF THESE AND PUT THEM IN AN ARRAY LIKE BELOW
+// function* watchIncrementAsync() {
+//   yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+// }
+
+// // notice how we now only export the rootSaga
+// // single entry point to start all Sagas at once
+// export default function* rootSaga() {
+//   yield all([
+//     helloSaga(),
+//     watchIncrementAsync()
+//   ])
+// }
+//************************************************** */
 
 //when a get user action is dispatched, watcher saga will
 //take that action and call get user, that get user will then call
